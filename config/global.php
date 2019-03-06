7db94da012a8e6d19fec239a409b38571970f438
qywuyanzu <1806804351@qq.com>
2019-03-06 21:47:21 +0800

    0.0.2

--- config/global.php
+++ config/global.php
@@ -7,6 +7,6 @@
  */
 return [
     'title' =>env('TITLE','教师评分系统'),
-    'version' =>env('VERSION','0.0.1'),
+    'version' =>env('VERSION','0.0.2'),
     'copyright' => env('COPYRIGHT','@ 2019 4Ark. 版权所有')
 ];
