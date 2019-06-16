ff1fab35bf354d28eb5ef1e2c65b6ce0757e20df
gd4ark <gd4ark@gmail.com>
2019-06-17 01:28:46 +0800

    update table components

--- src/common/components/Table.vue
+++ src/common/components/Table.vue
@@ -1,24 +1,24 @@
 <template>
-    <el-table v-loading="loading"
-              :data="data"
-              :height="height"
-              @selection-change="handleSelectionChange"
-              @sort-change="handleSortChange">
-        <el-table-column v-if="needSelection"
-                         type="selection"
-                         align="center" />
-        <slot name="prepend" />
-        <el-table-column v-for="(column,index) in columns"
-                         :key="index"
-                         v-bind="column"
-                         :render-header="column.isRender ? renderHeader : null"
-                         align="center">
-            <template slot-scope="scope">
-                <p>{{ scope.row[column.prop] }}</p>
-            </template>
-        </el-table-column>
-        <slot name="append" />
-    </el-table>
+  <el-table v-loading="loading"
+            :data="data"
+            fit
+            @selection-change="handleSelectionChange"
+            @sort-change="handleSortChange">
+    <el-table-column v-if="needSelection"
+                     type="selection"
+                     align="center" />
+    <slot name="prepend" />
+    <el-table-column v-for="(column,index) in columns"
+                     :key="index"
+                     v-bind="column"
+                     :render-header="column.isRender ? renderHeader : null"
+                     align="center">
+      <template slot-scope="scope">
+        <p>{{ scope.row[column.prop] }}</p>
+      </template>
+    </el-table-column>
+    <slot name="append" />
+  </el-table>
 </template>
 <script>
 export default {
@@ -28,13 +28,13 @@ export default {
             type: Array,
             required: true
         },
-        columns: {
-            type: Array,
-            required: true
-        },
         height: {
             type: String,
             default: '93%'
+        },
+        columns: {
+            type: Array,
+            required: true
         },
         loading: {
             type: Boolean,
@@ -61,9 +61,17 @@ export default {
 }
 .el-table {
     position: relative;
+    @include flex-column;
     font-size: $table-font-size;
     width: $table-width;
-    // height: $table-height;
+    height: $table-height;
+    .el-table__header-wrapper {
+        overflow: initial;
+    }
+    .el-table__body-wrapper {
+        flex: 1;
+        overflow: auto;
+    }
     a {
         color: $gighlight-color;
     }
