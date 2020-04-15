fb4dcb14b99143197e01f45532fb19d0131bbd5a
gd4ark <gd4ark@gmail.com>
2020-04-15 22:50:45 +0800

    fix: api 地址不正确

--- src/common/config/index.js
+++ src/common/config/index.js
@@ -3,7 +3,7 @@ export const app_name = process.env.APP_NAME
 export const app_title = process.env.APP_TITLE
 export const app_version = process.env.APP_VERSION
 export const app_copyright = 'Powered By 4Ark'
-export const dev_base_url = `http://127.0.0.1/teacher-scoring/server/public/`
+export const dev_base_url = `http://127.0.0.1/teacher-scoring-system/server/public/`
 export const base_url = `http://${location.hostname}:82/admin`
 
 export default {
